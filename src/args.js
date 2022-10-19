83e9887a6643b3959241e02980404fa39f026ac5
Adam Cheng <52572642+adamchengtkc@users.noreply.github.com>
2022-10-19 10:15:00 +0200

    fix when arg value include =

--- src/args.js
+++ src/args.js
@@ -4,19 +4,19 @@ export function parse() {
         let arg = args[i].split('=');
         switch (arg[0]) {
             case 'http_method':
-                process.env.HTTP_METHOD = arg[1];
+                process.env.HTTP_METHOD = arg.slice(1,);
                 break;
             case 'url':
-                process.env.URL = arg[1];
+                process.env.URL = arg.slice(1,);
                 break;
             case 'data':
-                process.env.DATA = arg[1];
+                process.env.DATA = arg.slice(1,);
                 break;
             case 'auth':
-                process.env.AUTH = arg[1];
+                process.env.AUTH = arg.slice(1,);
                 break;
             case 'extra_headers':
-                process.env.EXTRA_HEADERS = arg[1];
+                process.env.EXTRA_HEADERS = arg.slice(1,);
                 break;
             default:
         }
